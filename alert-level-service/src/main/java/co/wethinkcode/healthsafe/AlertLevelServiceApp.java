c21c8bb7adc44b11330c270967e32b9fba0227dc
nothando-debug <nndlovu426@gmail.com>
2026-09-10 13:07:17 +0200

    feat: added logic so that alert service is able to get alert-level

--- alert-level-service/src/main/java/co/wethinkcode/healthsafe/AlertLevelServiceApp.java
+++ alert-level-service/src/main/java/co/wethinkcode/healthsafe/AlertLevelServiceApp.java
@@ -1,13 +1,21 @@
 package co.wethinkcode.healthsafe;
 
 import io.javalin.Javalin;
-
+import java.util.*;
 public class AlertLevelServiceApp {
+
+    private static int emergencyStatus = 0;
 
     public static void main(String[] args) {
         Javalin app = Javalin.create().start(7032);
 
         app.get("/health", ctx -> ctx.result("OK"));
+
+        app.get("/alert-level", ctx -> {
+            ctx.json(Map.of("level", emergencyStatus));
+        });
+
+
 
         // TODO (Tracks the hospital Emergency Status (0-8, 8 = full Code Blue).)
         // Add domain endpoints for alert-level-service here.
