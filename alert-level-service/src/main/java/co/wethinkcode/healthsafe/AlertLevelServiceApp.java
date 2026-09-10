a9e6efb8796f49f38f7893547ffbfef7a736b119
nothando-debug <nndlovu426@gmail.com>
2026-09-10 13:09:37 +0200

    feat: added post endpoint so that alert level can be updated

--- alert-level-service/src/main/java/co/wethinkcode/healthsafe/AlertLevelServiceApp.java
+++ alert-level-service/src/main/java/co/wethinkcode/healthsafe/AlertLevelServiceApp.java
@@ -15,6 +15,26 @@ public class AlertLevelServiceApp {
             ctx.json(Map.of("level", emergencyStatus));
         });
 
+        app.post("/alert-level/{level}", ctx -> {
+            try {
+                // Extract the number from the URL path
+                int newLevel = Integer.parseInt(ctx.pathParam("level"));
+
+                // Enforce the business rule: must be between 0 and 8
+                if (newLevel >= 0 && newLevel <= 8) {
+                    emergencyStatus = newLevel;
+                    ctx.result("Emergency Status successfully updated to: " + emergencyStatus);
+                } else {
+                    ctx.status(400).result("Invalid level. Must be between 0 and 8.");
+                }
+            } catch (NumberFormatException e) {
+                // Catch if someone types /alert-level/five
+                ctx.status(400).result("Level must be a valid number.");
+            }
+        });
+
+
+
 
 
         // TODO (Tracks the hospital Emergency Status (0-8, 8 = full Code Blue).)
