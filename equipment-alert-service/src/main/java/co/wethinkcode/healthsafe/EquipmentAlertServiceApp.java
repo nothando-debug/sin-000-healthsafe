9e7eac98fa75393eec3afab698b427ec5645f934
nothando-debug <nndlovu426@gmail.com>
2026-09-21 13:46:33 +0200

    feat: implemendted satge 4 alert serive and failure queue

--- equipment-alert-service/src/main/java/co/wethinkcode/healthsafe/EquipmentAlertServiceApp.java
+++ equipment-alert-service/src/main/java/co/wethinkcode/healthsafe/EquipmentAlertServiceApp.java
@@ -1,7 +1,11 @@
 package co.wethinkcode.healthsafe;
 
+import org.apache.activemq.ActiveMQConnectionFactory;
+
+import co.wethinkcode.healthsafe.mq.MqConfig;
 import io.javalin.Javalin;
 
+import jakarta.jms.*;
 public class EquipmentAlertServiceApp {
 
     public static void main(String[] args) {
@@ -9,8 +13,48 @@ public class EquipmentAlertServiceApp {
 
         app.get("/health", ctx -> ctx.result("OK"));
 
+        startQueueConsumer();
+        
+
         // TODO (Uses a Queue to guarantee delivery of critical medical equipment failure alerts.)
         // Mechanism: ActiveMQ Queue (guaranteed delivery)
+    }
+
+    private static void startQueueConsumer() {
+        try {
+            ConnectionFactory connectionFactory = new ActiveMQConnectionFactory(MqConfig.BROKER_URL);
+            Connection connection = connectionFactory.createConnection();
+            connection.start();
+
+            Session session = connection.createSession(false, Session.AUTO_ACKNOWLEDGE);
+            
+            Destination destination = session.createQueue(MqConfig.QUEUE);
+            MessageConsumer consumer = session.createConsumer(destination);
+
+            consumer.setMessageListener(new MessageListener() {
+                @Override
+                public void onMessage(Message message) {
+                    if (message instanceof TextMessage) {
+                        try {
+                            String failurePayload = ((TextMessage) message).getText();
+                            
+                            System.out.println("\n--------------------------------------------------");
+                            System.out.println("[EQUIPMENT ALERT SERVICE] MAINTENANCE ALERT RECEIVED!");
+                            System.out.println("Payload: " + failurePayload);
+                            System.out.println("--------------------------------------------------\n");
+
+                        } catch (JMSException e) {
+                            System.err.println("Error reading message from failure queue: " + e.getMessage());
+                        }
+                    }
+                }
+            });
+
+            System.out.println("Equipment Alert Service successfully subscribed to queue: " + MqConfig.QUEUE);
+
+        } catch (Exception e) {
+            System.err.println("Failed to start MQ Queue Consumer: " + e.getMessage());
+        }
     }
 }
 
